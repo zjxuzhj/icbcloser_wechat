4b93f520f3ea9104b99b108ec57b835841babee4
hongjay <hongjay@gmail.com>
2018-12-25 01:17:56 +0800

    视频

--- pages/video_detail/video_detail.js
+++ pages/video_detail/video_detail.js
@@ -1,80 +1,71 @@
-// pages/home.js
+// pages/img_detail/img_detail.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    updateString:"v1.0.6 新增个人中心，首页添加抽将视频页 \n\nv1.0.5 新增更新日志，添加手游观察周刊关注位置 \n\nv1.0.4 新增底部科技、标配等栏目分类"
+    videoBean:[]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+      var that=this
+    var videoBean = JSON.parse(options.videoBean);
+    console.log(videoBean)
+      that.setData({
+        videoBean: videoBean
+      })
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
+  
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+  
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-
+  
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-
+  
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-
+  
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-
+  
   },
 
   /**
-   * 跳转抽将视频列表
+   * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-
-  },
-  onChooseVideo: function () {
-    wx.navigateTo({
-      url: '../videolist/videolist',
-      success: function (res) {
-        // success
-      },
-      fail: function () {
-        // fail
-      },
-      complete: function () {
-        // complete
-      }
-    })
-  },
+  
+  }
 })
